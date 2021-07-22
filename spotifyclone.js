c0af1014b5085d1f3b111086d99d5b7b1d659b07
barrytrearty <btrearty@gmail.com>
2021-07-22 08:24:57 +0100

    Added js

---
+++ spotifyclone.js
@@ -0,0 +1,22 @@
+"strict mode";
+
+const album1 = {
+  name: "Bohemian Rhapsody (The Original Soundtrack)",
+  artist: "Queen",
+  year: "2018",
+  duration: "22 Songs, 1 hr 19 min",
+  songList: [
+    "20th Century Fox Fanfare",
+    "Somebody to Love",
+    "Doing All Right... Revisited",
+    "Keep Yourself Alive",
+    "Killer Queen",
+    "Fat Bottomed Girls",
+    "Bohemian Rhapsody",
+    "Now I'm Here",
+    "Crazy Little Thing Called Love",
+    "Love of My Life",
+    "We Will Rock You",
+    "Another One Bites the Dust",
+  ],
+};
