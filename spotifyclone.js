afee1d08cda05feed18f08491d7304190c647e79
barrytrearty <btrearty@gmail.com>
2021-07-22 15:54:57 +0100

    created functions that render album tracklist

--- spotifyclone.js
+++ spotifyclone.js
@@ -4,6 +4,7 @@ const album1 = {
   name: "Bohemian Rhapsody (The Original Soundtrack)",
   artist: "Queen",
   year: "2018",
+  albumCover: "electric-warrior.png",
   duration: "22 Songs, 1 hr 19 min",
   songList: [
     {
@@ -122,3 +123,169 @@ const album1 = {
     },
   ],
 };
+
+const album2 = {
+  name: "News of the World",
+  artist: "Queen",
+  year: "1977",
+  albumCover: "dogrel.png",
+  duration: "11 Songs, 39 min",
+  songList: [
+    {
+      title: "We Will Rock You",
+      songDuration: "2:01",
+      songArtist: "Queen",
+    },
+    {
+      title: "We Are the Champions",
+      songDuration: "2:59",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Sheer Heart Attack",
+      songDuration: "3:26",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "All Dead, All Dead",
+      songDuration: "3:10",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Spread Your Wings",
+      songDuration: "4:34",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Fight from the Inside",
+      songDuration: "4.38",
+      songArtist: "Queen",
+    },
+    {
+      title: "Get Down, Make Love",
+      songDuration: "5.55",
+      songArtist: "Queen",
+    },
+    {
+      title: "Sleeping on the Sidewalk",
+      songDuration: "4.26",
+      songArtist: "Queen",
+    },
+    {
+      title: "Who Needs You",
+      songDuration: "2.43",
+      songArtist: "Queen",
+    },
+    {
+      title: "It's Late",
+      songDuration: "4.29",
+      songArtist: "Queen",
+    },
+    {
+      title: "My Melancholy Blues",
+      songDuration: "2.09",
+      songArtist: "Queen",
+    },
+  ],
+};
+
+const generateAlbumPageJumbotron = function (album, container) {
+  let newJumbotron = document.createElement("div");
+  newJumbotron.innerHTML = `
+  <div class="jumbotron jumbotron-fluid img-fluid album-jumbo">
+    <div class="for-about-album">
+      <img class="shadow-lg d-none d-sm-inline" src="assets/${album.albumCover}" alt="">
+      <div class="mt-3 mt-sm-0">
+        <p><strong>ALBUM</strong> </p>
+        <h1 class="album-title-jumbo">${album.name}</h1>
+        
+        <p class="album-summary"><img class="round-artist1" src="assets/mgmt.png" alt=""><strong> ${album.artist}</strong><i class="bi bi-dot"></i>2018<i class="bi bi-dot"></i>${album.duration}</p>
+      </div>
+    </div>
+   
+ </div>`;
+
+  container.appendChild(newJumbotron);
+};
+
+const generateAlbumTable = function (albumTitle) {
+  let albumTableBody = document.createElement("tbody");
+  for (i = 0; i < albumTitle.songList.length; i++) {
+    let newTableRow = document.createElement("tr");
+    newTableRow.innerHTML = `<td scope="row" class="align-middle"> ${i + 1}</td>
+      
+    <td><div>${
+      albumTitle.songList[i].title
+    } </div> <div class="artist-name">Queen</div> </td>
+    
+    <td class="align-middle keep-on-page">${
+      albumTitle.songList[i].songDuration
+    }</td>`;
+    albumTableBody.appendChild(newTableRow);
+  }
+  return albumTableBody;
+};
+
+const generateTrackList = function (album) {
+  let albumTableContainer = document.createElement("div");
+  albumTableContainer.classList.add(
+    "main-container",
+    "main-container-album",
+    "container-fluid",
+    "album-songs-container"
+  );
+  albumTableContainer.innerHTML = `  <div class="top-of-artist-songs mb-3 pt-3">
+  <div class="spotify-circle">
+    <div class="half-of-pause first-pause"></div>
+    <div class="half-of-pause second-pause"></div>
+  </div>
+  <div class="album-heart"><i class="bi bi-heart"></i></div>
+  <div class="album-dots"> <div class="small-circle"></div>
+  <div class="small-circle"></div>
+    <div class="small-circle"></div></div>
+</div>`;
+  let tableBootRow = document.createElement("div");
+  tableBootRow.classList.add("row", "ml-1");
+  let table = document.createElement("table");
+  table.classList.add("table", "table-borderless");
+  table.innerHTML = `  <thead>
+  <tr>
+  <td>#</td>
+  <td>TITLE</td>
+  <td><i class="bi bi-clock keep-on-page-clock"></i></td>
+</tr>
+</thead>`;
+  let tableBody = generateAlbumTable(album);
+  table.appendChild(tableBody);
+  tableBootRow.appendChild(table);
+  albumTableContainer.appendChild(tableBootRow);
+  let albumTableContainerDiv = document.getElementById(
+    "album-table-container-div"
+  );
+  albumTableContainerDiv.appendChild(albumTableContainer);
+};
+
+// let albumName;
+
+const generateAlbum = function (album) {
+  let albumJumbotronContainer = document.getElementById(
+    "album-jumbotron-container"
+  );
+  //   console.log(albumJumbotronContainer);
+  generateAlbumPageJumbotron(album, albumJumbotronContainer);
+  generateTrackList(album);
+};
+
+// const generateNewsAlbum = function () {
+//   window.location.href = "album.html";
+//   let albumJumbotronContainer = document.getElementById(
+//     "album-jumbotron-container"
+//   );
+//   //   console.log(albumJumbotronContainer);
+//   generateAlbumPageJumbotron(album1, albumJumbotronContainer);
+//   generateTrackList(album1);
+// };
