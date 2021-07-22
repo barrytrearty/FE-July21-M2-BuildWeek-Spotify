cbfe7da390da3eb37f637113494cfa78621e71c6
barrytrearty <btrearty@gmail.com>
2021-07-22 09:15:52 +0100

    Added classes and ids

--- spotifyclone.js
+++ spotifyclone.js
@@ -6,17 +6,119 @@ const album1 = {
   year: "2018",
   duration: "22 Songs, 1 hr 19 min",
   songList: [
-    "20th Century Fox Fanfare",
-    "Somebody to Love",
-    "Doing All Right... Revisited",
-    "Keep Yourself Alive",
-    "Killer Queen",
-    "Fat Bottomed Girls",
-    "Bohemian Rhapsody",
-    "Now I'm Here",
-    "Crazy Little Thing Called Love",
-    "Love of My Life",
-    "We Will Rock You",
-    "Another One Bites the Dust",
+    {
+      title: "20th Century Fox Fanfare",
+      songDuration: "0.25",
+      songArtist: "Queen",
+    },
+    {
+      title: "Somebody to Love",
+      songDuration: "4.56",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Doing All Right... Revisited",
+      songDuration: "3.17",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Keep Yourself Alive",
+      songDuration: "3.56",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Killer Queen",
+      songDuration: "2.59",
+      songArtist: "Queen",
+    },
+
+    {
+      title: "Fat Bottomed Girls",
+      songDuration: "4.38",
+      songArtist: "Queen",
+    },
+    {
+      title: "Bohemian Rhapsody",
+      songDuration: "5.55",
+      songArtist: "Queen",
+    },
+    {
+      title: "Now I'm Here",
+      songDuration: "4.26",
+      songArtist: "Queen",
+    },
+    {
+      title: "Crazy Little Thing Called Love",
+      songDuration: "2.43",
+      songArtist: "Queen",
+    },
+    {
+      title: "Love of My Life",
+      songDuration: "4.29",
+      songArtist: "Queen",
+    },
+    {
+      title: "We Will Rock You",
+      songDuration: "2.09",
+      songArtist: "Queen",
+    },
+    {
+      title: "Another One Bites the Dust",
+      songDuration: "3.35",
+      songArtist: "Queen",
+    },
+    {
+      title: "I Want to Break Free",
+      songDuration: "3.43",
+      songArtist: "Queen",
+    },
+    {
+      title: "Under Pressure",
+      songDuration: "4.04",
+      songArtist: "Queen feat. David Bowie",
+    },
+    {
+      title: "Who Wants to Live Forever",
+      songDuration: "5.15",
+      songArtist: "Queen",
+    },
+    {
+      title: "Bohemian Rhapsody(Live)",
+      songDuration: "2.28",
+      songArtist: "Queen",
+    },
+    {
+      title: "Radio Ga Ga",
+      songDuration: "4:06",
+      songArtist: "Queen",
+    },
+    {
+      title: "Ay-Oh",
+      songDuration: "0:41",
+      songArtist: "Queen",
+    },
+    {
+      title: "Hammer to Fall",
+      songDuration: "4:04",
+      songArtist: "Queen",
+    },
+    {
+      title: "We Are the Champions",
+      songDuration: "3:57",
+      songArtist: "Queen",
+    },
+    {
+      title: "Don't Stop Me Now... Revisited",
+      songDuration: "3:38",
+      songArtist: "Queen",
+    },
+    {
+      title: "The Show Must Go On",
+      songDuration: "4:32",
+      songArtist: "Queen",
+    },
   ],
 };
